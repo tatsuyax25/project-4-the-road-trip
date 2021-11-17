c010ed5e4c03ebcc0d41f0515e0d4ec6d7afce1f
Miguel Angel Urena <urena_miguel82@outlook.com>
2021-11-16 19:49:53 -0800

    Add postApi.js

---
+++ src/utils/postApi.js
@@ -0,0 +1,28 @@
+import tokenService from "./tokenService";
+
+const BASE_URL = "/api/posts/";
+
+export function create(post){
+    return fetch(BASE_URL, {
+        method: 'POST',
+        body: post,
+        headers: {
+            'Authorization': 'Bearer ' + tokenService.getToken()
+        }
+    }).then(res => {
+        if (res.ok) return res.json();
+        throw new Error('Bad Credentials');
+    })
+}
+
+export function getAll() {
+    return fetch (BASE_URL, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + tokenService.getToken()
+        }
+    }).then(res => {
+        if (res.ok) return res.json();
+        throw new Error('Bad Credentials');
+    })
+}
