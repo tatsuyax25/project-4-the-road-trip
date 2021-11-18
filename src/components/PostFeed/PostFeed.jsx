c78b2838e822d415c792b0ef059c8a71622de071
Miguel Angel Urena <urena_miguel82@outlook.com>
2021-11-18 09:32:56 -0800

    Add PostFeed.jsx

---
+++ src/components/PostFeed/PostFeed.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, Loader, Dimmer, Segment, Image } from 'semantic-ui-react';
+import PostCard from '../PostCard/PostCard';
+
+export default function PostFeed({
+    posts,
+    numPhotosCol,
+    isProfile,
+    loading,
+    user,
+    addLike,
+    removeLike
+}) {
+    return (
+        <Card.Group itemsPerRow={numPhotosCol} stackable>
+            {loading ? (
+                <Segment>
+                    <Dimmer active inverted>
+                        <Loader size="small">Loading</Loader>
+                    </Dimmer>
+                    <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+                </Segment>
+            ) : null}
+            {posts.map((post) => {
+                return (
+                    <PostCard
+                        post={post}
+                        key={post._id}
+                        isProfile={isProfile}
+                        user={user}
+                        removeLike={removeLike}
+                        addLike={addLike}
+                    />
+                );
+            })}
+        </Card.Group>
+    )
+}
