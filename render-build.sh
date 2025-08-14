b1064e1afd7be9a97c8687311d6aadeb70aa9bad
Miguel Urena <urena_miguel82@outlook.com>
2025-08-14 13:17:06 -0700

    Fixing application

---
+++ render-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Build script for Render deployment
+
+# Install dependencies
+npm install
+
+# Build the React app
+npm run build
--- server.js
+++ server.js
@@ -10,8 +10,10 @@ require('./config/database');
 
 const app = express();
 
-// add in when the app is ready to be deployed
-// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// Serve favicon in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+}
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
